7a2a5eace9640873f140d3e4ccc5a477e62bda8f
Girinath V <girinath439@gmail.com>
2026-02-04 10:29:54 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 setup(
     name="omnirag",
-    version="1.0.2",
+    version="1.0.3",
     author="Girinath V",
     author_email="girinathv48@gmail.com",
     description="OmniRAG: Universal RAG System combining Liquid + Agentic + Chain RAG",
